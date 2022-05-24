c31390bc0aa1db468fd763267c7b313082bbdcff
Jakob Braun <jakob.braun@posteo.de>
2022-05-24 05:55:53 -0300

    fixes

--- backend/integrationTesting/extensionForTestingFixture.go
+++ backend/integrationTesting/extensionForTestingFixture.go
@@ -15,18 +15,20 @@ func GetExtensionForTesting(pathToProjectRoot string) string {
 	extensionForTestingDir := path.Join(pathToProjectRoot, "integrationTesting", "extensionForTesting")
 	if !isBuilt {
 		isBuilt = true
+		var stderr bytes.Buffer
 		installCommand := exec.Command("npm", "ci")
+		installCommand.Stderr = &stderr
 		installCommand.Dir = extensionForTestingDir
 		err := installCommand.Run()
 		if err != nil {
+			fmt.Println(stderr.String())
 			panic(fmt.Sprintf("Failed to install node modules (run 'npm ci') for extensionForTesting. Cause: %v", err.Error()))
 		}
 		buildCommand := exec.Command("npm", "run", "build")
+		buildCommand.Stderr = &stderr
 		buildCommand.Dir = extensionForTestingDir
 		err = buildCommand.Run()
 		if err != nil {
-			var stderr bytes.Buffer
-			buildCommand.Stderr = &stderr
 			fmt.Println(stderr.String())
 			panic(fmt.Sprintf("Failed to build extensionForTesting. Cause: %v", err.Error()))
 		}
