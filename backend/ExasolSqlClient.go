a12621c7b166964da258c5f6020e265381970803
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-07-27 10:41:26 +0200

    Log result of executed query

--- backend/ExasolSqlClient.go
+++ backend/ExasolSqlClient.go
@@ -11,10 +11,14 @@ type ExasolSqlClient struct {
 }
 
 func (client ExasolSqlClient) RunQuery(query string) {
-	log.Printf("Executing statement %q", query)
-	_, err := client.Connection.Exec(query)
+	result, err := client.Connection.Exec(query)
 	if err != nil {
 		// Panic to signal a failed query to the JavaScript extension code.
 		panic(fmt.Sprintf("error executing statement %q: %v", query, err))
 	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		panic(fmt.Sprintf("error getting rows affected for statement %q: %v", query, err))
+	}
+	log.Printf("Executed statement %q: rows affected: %d", query, rowsAffected)
 }
