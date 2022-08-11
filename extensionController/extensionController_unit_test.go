cae3e8663c2a38fe8c442a80bd74a8c302e8b97f
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-08-11 16:44:06 +0200

    Add unit tests for extension controller

---
+++ extensionController/extensionController_unit_test.go
@@ -0,0 +1,132 @@
+package extensionController
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/exasol/extension-manager/extensionAPI"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type mockControllerImpl struct {
+	mock.Mock
+}
+
+func (mock *mockControllerImpl) GetAllExtensions(bfsFiles []BfsFile) ([]*Extension, error) {
+	args := mock.Called(bfsFiles)
+	if ext, ok := args.Get(0).([]*Extension); ok {
+		return ext, args.Error(1)
+	} else {
+		return nil, args.Error(1)
+	}
+}
+func (mock *mockControllerImpl) GetAllInstallations(tx *sql.Tx) ([]*extensionAPI.JsExtInstallation, error) {
+	args := mock.Called(tx)
+	return args.Get(0).([]*extensionAPI.JsExtInstallation), args.Error(1)
+}
+func (mock *mockControllerImpl) InstallExtension(tx *sql.Tx, extensionId string, extensionVersion string) error {
+	args := mock.Called(tx, extensionId, extensionVersion)
+	return args.Error(0)
+}
+func (mock *mockControllerImpl) CreateInstance(tx *sql.Tx, extensionId string, extensionVersion string, parameterValues []ParameterValue) (string, error) {
+	args := mock.Called(tx, extensionId, extensionVersion, parameterValues)
+	return args.String(0), args.Error(1)
+}
+
+type mockBucketFs struct {
+	mock.Mock
+}
+
+func (mock *mockBucketFs) ListBuckets(ctx context.Context, db *sql.DB) ([]string, error) {
+	args := mock.Called(ctx, db)
+	if buckets, ok := args.Get(0).([]string); ok {
+		return buckets, args.Error(1)
+	} else {
+		return args.Get(0).([]string), args.Error(1)
+	}
+}
+func (mock *mockBucketFs) ListFiles(ctx context.Context, db *sql.DB, bucket string) ([]BfsFile, error) {
+	args := mock.Called(ctx, db, bucket)
+	if files, ok := args.Get(0).([]BfsFile); ok {
+		return files, args.Error(1)
+	} else {
+		return nil, args.Error(1)
+	}
+}
+
+type extCtrlUnitTestSuite struct {
+	suite.Suite
+	ctrl     ExtensionController
+	db       *sql.DB
+	dbMock   sqlmock.Sqlmock
+	mockCtrl mockControllerImpl
+	mockBfs  mockBucketFs
+}
+
+func TestExtensionControllerUnitTestSuite(t *testing.T) {
+	suite.Run(t, new(extCtrlUnitTestSuite))
+}
+
+func (suite *extCtrlUnitTestSuite) SetupTest() {
+	suite.mockCtrl = mockControllerImpl{}
+	suite.mockBfs = mockBucketFs{}
+	suite.ctrl = &extensionControllerImpl{impl: &suite.mockCtrl, bfsAPI: &suite.mockBfs}
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		suite.Failf("error '%v' was not expected when opening a stub database connection", err.Error())
+	}
+	suite.db = db
+	suite.dbMock = mock
+}
+
+func (suite *extCtrlUnitTestSuite) TestGetAllExtensions_Success() {
+	suite.mockBfs.On("ListFiles", mock.Anything, mock.Anything, "default").Return([]BfsFile{}, nil)
+	suite.mockCtrl.On("GetAllExtensions", mock.Anything).Return([]*Extension{}, nil)
+	extensions, err := suite.ctrl.GetAllExtensions(mockContext(), suite.db)
+	suite.NoError(err)
+	suite.Len(extensions, 0)
+}
+
+func (suite *extCtrlUnitTestSuite) TestGetAllExtensions_BucketFsListFails() {
+	suite.mockBfs.On("ListFiles", mock.Anything, mock.Anything, "default").Return(nil, fmt.Errorf("mock"))
+	extensions, err := suite.ctrl.GetAllExtensions(mockContext(), suite.db)
+	suite.EqualError(err, "failed to search for required files in BucketFS. Cause: mock")
+	suite.Nil(extensions)
+}
+
+func (suite *extCtrlUnitTestSuite) TestGetAllExtensions_GetFails() {
+	suite.mockBfs.On("ListFiles", mock.Anything, mock.Anything, "default").Return([]BfsFile{}, nil)
+	suite.mockCtrl.On("GetAllExtensions", mock.Anything).Return(nil, fmt.Errorf("mock"))
+	extensions, err := suite.ctrl.GetAllExtensions(mockContext(), suite.db)
+	suite.EqualError(err, "mock")
+	suite.Nil(extensions)
+}
+
+func (suite *extCtrlUnitTestSuite) TestInstallExtension_Success() {
+	suite.dbMock.ExpectBegin()
+	suite.mockCtrl.On("InstallExtension", mock.Anything, "extId", "extVer").Return(nil)
+	suite.dbMock.ExpectCommit()
+	err := suite.ctrl.InstallExtension(mockContext(), suite.db, "extId", "extVer")
+	suite.NoError(err)
+}
+
+func (suite *extCtrlUnitTestSuite) TestInstallExtension_FailureRollback() {
+	suite.dbMock.ExpectBegin()
+	suite.mockCtrl.On("InstallExtension", mock.Anything, "extId", "extVer").Return(fmt.Errorf("mock"))
+	suite.dbMock.ExpectRollback()
+	err := suite.ctrl.InstallExtension(mockContext(), suite.db, "extId", "extVer")
+	suite.EqualError(err, "mock")
+}
+
+func (suite *extCtrlUnitTestSuite) TestInstallExtension_CommitFailure() {
+	suite.dbMock.ExpectBegin()
+	suite.mockCtrl.On("InstallExtension", mock.Anything, "extId", "extVer").Return(nil)
+	suite.dbMock.ExpectCommit().WillReturnError(fmt.Errorf("commit failed"))
+	err := suite.ctrl.InstallExtension(mockContext(), suite.db, "extId", "extVer")
+	suite.EqualError(err, "commit failed")
+}
