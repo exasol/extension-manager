6a4a55e600d13cb620a18d78169c40698fe3928f
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-11-03 13:25:30 +0100

    Upgrade oft

--- ci/trace-requirements.sh
+++ ci/trace-requirements.sh
@@ -4,22 +4,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-oft_version="3.5.0"
+oft_version="3.6.0"
 tmp_dir="/tmp/oft/"
-tmp_file="$tmp_dir/openfasttrace-$oft_version.jar"
+jar_file="$tmp_dir/openfasttrace-$oft_version.jar"
 
 base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 readonly base_dir
 
-if [[ ! -f "$tmp_file" ]]; then
+if [[ ! -f "$jar_file" ]]; then
     mkdir -p "$tmp_dir"
     url="https://repo1.maven.org/maven2/org/itsallcode/openfasttrace/openfasttrace/$oft_version/openfasttrace-$oft_version.jar"
-    echo "Downloading $url to $tmp_file"
-    curl --output "$tmp_file" "$url"
+    echo "Downloading $url to $jar_file"
+    curl --output "$jar_file" "$url"
 fi
 
 # Trace all
-#java -jar "$tmp_file" trace "$base_dir/doc"
+#java -jar "$jar_file" trace "$base_dir/doc"
 
 # Trace only feat,req,dsn
-java -jar "$tmp_file" trace --wanted-artifact-types feat,req,dsn "$base_dir/doc"
+java -jar "$jar_file" trace --wanted-artifact-types feat,req,dsn "$base_dir/doc"
