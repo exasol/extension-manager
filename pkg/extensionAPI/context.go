917208441646b0528dd7ef48fae71839cf1e0800
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-07-19 08:26:43 +0200

    #116: Add transaction context

--- pkg/extensionAPI/context.go
+++ pkg/extensionAPI/context.go
@@ -7,7 +7,10 @@ import (
 	"github.com/exasol/extension-manager/pkg/backend"
 )
 
-const BUCKETFS_PATH_PREFIX = "/buckets/bfsdefault/default/"
+func CreateContext(ctx context.Context, extensionSchemaName string, tx *sql.Tx, db *sql.DB) *ExtensionContext {
+	var client SimpleSQLClient = backend.NewSqlClient(ctx, tx)
+	return CreateContextWithClient(extensionSchemaName, client)
+}
 
 func CreateContextWithClient(extensionSchemaName string, client SimpleSQLClient) *ExtensionContext {
 	return &ExtensionContext{
@@ -17,23 +20,24 @@ func CreateContextWithClient(extensionSchemaName string, client SimpleSQLClient)
 	}
 }
 
-func CreateContext(ctx context.Context, extensionSchemaName string, tx *sql.Tx) *ExtensionContext {
-	var client SimpleSQLClient = backend.NewSqlClient(ctx, tx)
-	return CreateContextWithClient(extensionSchemaName, client)
-}
-
+// Instances of type ExtensionContext are passed to an extension so that extension can
+//   - retrieve context information like the extension schema name (field ExtensionSchemaName)
+//   - execute SQL queries against the database using a [SqlClient]
+//   - or resolve files in BucketFS using [BucketFs]
 type ExtensionContext struct {
-	ExtensionSchemaName string          `json:"extensionSchemaName"`
-	BucketFs            BucketFsContext `json:"bucketFs"`
-	SqlClient           SimpleSQLClient `json:"sqlClient"`
+	ExtensionSchemaName string          `json:"extensionSchemaName"` // Name of the schema where EM creates all database objects (e.g. scripts or virtual schemas)
+	SqlClient           SimpleSQLClient `json:"sqlClient"`           // Allows extensions to execute SQL queries and statements
+	BucketFs            BucketFsContext `json:"bucketFs"`            // Allows extensions to interact with BucketFS
 }
 
+// BucketFsContext allows extensions to interact with BucketFS
 type BucketFsContext interface {
+	// ResolvePath returns an absolute path for the given filename in BucketFS.
 	ResolvePath(fileName string) string
 }
 
 type bucketFsContextImpl struct{}
 
 func (b *bucketFsContextImpl) ResolvePath(fileName string) string {
-	return BUCKETFS_PATH_PREFIX + fileName
+	return "/buckets/bfsdefault/default/" + fileName
 }
