ec5035f7a1c68ad44af172bdcc0c6cde6c5cbe66
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-07-20 15:54:53 +0200

    Move BucketFS itests to itest file

--- pkg/extensionController/bfs/BucketFsApi_i_test.go
+++ pkg/extensionController/bfs/BucketFsApi_i_test.go
@@ -1,4 +1,4 @@
-package bfs
+package bfs_test
 
 import (
 	"context"
@@ -7,30 +7,31 @@ import (
 
 	"testing"
 
+	"github.com/exasol/extension-manager/pkg/extensionController/bfs"
 	"github.com/exasol/extension-manager/pkg/integrationTesting"
 	"github.com/stretchr/testify/suite"
 )
 
 const DEFAULT_BUCKET_PATH = "/buckets/bfsdefault/default/"
 
-type BucketFsAPISuite struct {
+type BucketFsAPIITestSuite struct {
 	suite.Suite
 	exasol integrationTesting.DbTestSetup
 }
 
-func TestBucketFsApiSuite(t *testing.T) {
-	suite.Run(t, new(BucketFsAPISuite))
+func TestBucketFsApiITestSuite(t *testing.T) {
+	suite.Run(t, new(BucketFsAPIITestSuite))
 }
 
-func (suite *BucketFsAPISuite) SetupSuite() {
+func (suite *BucketFsAPIITestSuite) SetupSuite() {
 	suite.exasol = *integrationTesting.StartDbSetup(&suite.Suite)
 }
 
-func (suite *BucketFsAPISuite) TeardownSuite() {
+func (suite *BucketFsAPIITestSuite) TeardownSuite() {
 	suite.exasol.StopDb()
 }
 
-func (suite *BucketFsAPISuite) BeforeTest(suiteName, testName string) {
+func (suite *BucketFsAPIITestSuite) BeforeTest(suiteName, testName string) {
 	suite.exasol.CreateConnection()
 	suite.T().Cleanup(func() {
 		suite.exasol.CloseConnection()
@@ -38,22 +39,22 @@ func (suite *BucketFsAPISuite) BeforeTest(suiteName, testName string) {
 }
 
 /* [utest -> dsn~extension-components~1] */
-func (suite *BucketFsAPISuite) TestListEmptyDir() {
+func (suite *BucketFsAPIITestSuite) TestListEmptyDir() {
 	result, err := suite.listFiles()
 	suite.NoError(err)
 	suite.Empty(result)
 }
 
-func (suite *BucketFsAPISuite) TestListSingleFile() {
+func (suite *BucketFsAPIITestSuite) TestListSingleFile() {
 	fileName := fmt.Sprintf("myFile-%d.txt", time.Now().Unix())
 	suite.uploadStringContent(fileName, "12345")
 	result, err := suite.listFiles()
 	suite.NoError(err)
 	suite.Len(result, 1)
-	suite.Equal([]BfsFile{{Name: fileName, Path: DEFAULT_BUCKET_PATH + fileName, Size: 5}}, result)
+	suite.Equal([]bfs.BfsFile{{Name: fileName, Path: DEFAULT_BUCKET_PATH + fileName, Size: 5}}, result)
 }
 
-func (suite *BucketFsAPISuite) TestListFilesRecursively() {
+func (suite *BucketFsAPIITestSuite) TestListFilesRecursively() {
 	file1 := "file1"
 	file2 := "dir1/file2"
 	file3 := "dir2/file2"
@@ -63,20 +64,20 @@ func (suite *BucketFsAPISuite) TestListFilesRecursively() {
 	result, err := suite.listFiles()
 	suite.NoError(err)
 	suite.Len(result, 3)
-	suite.Equal([]BfsFile{
+	suite.Equal([]bfs.BfsFile{
 		{Name: "file2", Path: DEFAULT_BUCKET_PATH + file2, Size: 2},
 		{Name: "file2", Path: DEFAULT_BUCKET_PATH + file3, Size: 3},
 		{Name: "file1", Path: DEFAULT_BUCKET_PATH + file1, Size: 1}}, result)
 }
 
-func (suite *BucketFsAPISuite) TestFindAbsolutePathNoFileFound() {
+func (suite *BucketFsAPIITestSuite) TestFindAbsolutePathNoFileFound() {
 	time.Sleep(3 * time.Second)
 	result, err := suite.findAbsolutePath("no-such-file")
 	suite.EqualError(err, `file "no-such-file" not found in BucketFS`)
 	suite.Equal("", result)
 }
 
-func (suite *BucketFsAPISuite) TestFindAbsolutePathFileInRoot() {
+func (suite *BucketFsAPIITestSuite) TestFindAbsolutePathFileInRoot() {
 	fileName := "file.txt"
 	suite.uploadStringContent(fileName, "123")
 	result, err := suite.findAbsolutePath(fileName)
@@ -84,14 +85,14 @@ func (suite *BucketFsAPISuite) TestFindAbsolutePathFileInRoot() {
 	suite.Equal("/buckets/bfsdefault/default/"+fileName, result)
 }
 
-func (suite *BucketFsAPISuite) TestFindAbsolutePathFileInSubDir() {
+func (suite *BucketFsAPIITestSuite) TestFindAbsolutePathFileInSubDir() {
 	suite.uploadStringContent("dir/file.txt", "123")
 	result, err := suite.findAbsolutePath("file.txt")
 	suite.NoError(err)
 	suite.Equal("/buckets/bfsdefault/default/dir/file.txt", result)
 }
 
-func (suite *BucketFsAPISuite) TestFindAbsolutePathMultipleFiles() {
+func (suite *BucketFsAPIITestSuite) TestFindAbsolutePathMultipleFiles() {
 	suite.uploadStringContent("dirA/file.txt", "123")
 	suite.uploadStringContent("dirB/file.txt", "98765")
 	result, err := suite.findAbsolutePath("file.txt")
@@ -99,17 +100,17 @@ func (suite *BucketFsAPISuite) TestFindAbsolutePathMultipleFiles() {
 	suite.Equal("/buckets/bfsdefault/default/dirA/file.txt", result)
 }
 
-func (suite *BucketFsAPISuite) listFiles() ([]BfsFile, error) {
-	bfsClient := CreateBucketFsAPI(DEFAULT_BUCKET_PATH)
+func (suite *BucketFsAPIITestSuite) listFiles() ([]bfs.BfsFile, error) {
+	bfsClient := bfs.CreateBucketFsAPI(DEFAULT_BUCKET_PATH)
 	return bfsClient.ListFiles(context.Background(), suite.exasol.GetConnection())
 }
 
-func (suite *BucketFsAPISuite) findAbsolutePath(fileName string) (string, error) {
-	bfsClient := CreateBucketFsAPI(DEFAULT_BUCKET_PATH)
+func (suite *BucketFsAPIITestSuite) findAbsolutePath(fileName string) (string, error) {
+	bfsClient := bfs.CreateBucketFsAPI(DEFAULT_BUCKET_PATH)
 	return bfsClient.FindAbsolutePath(context.Background(), suite.exasol.GetConnection(), fileName)
 }
 
-func (suite *BucketFsAPISuite) uploadStringContent(fileName string, fileContent string) {
+func (suite *BucketFsAPIITestSuite) uploadStringContent(fileName string, fileContent string) {
 	err := suite.exasol.Exasol.UploadStringContent(fileContent, fileName)
 	if err != nil {
 		suite.FailNowf("Failed to upload file %q. Cause: %w", fileName, err)
