ee801d0e957dc1b72eddc33c782d26feba6f653a
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-09-14 14:57:57 +0200

    Revert "Upgrade to Exasol DB 7.1.23"
    This reverts commit bc12df143c72ec9b2dfcd4cc97624ae9ce34b05f.

--- pkg/integrationTesting/dbTestSetup.go
+++ pkg/integrationTesting/dbTestSetup.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-const defaultExasolDbVersion = "7.1.23"
+const defaultExasolDbVersion = "7.1.22"
 
 type DbTestSetup struct {
 	suite          *suite.Suite
