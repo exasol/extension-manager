d821fbede82976905cd0aa0127856ec6c8f0b412
Christoph Pirkl <christoph.pirkl@exasol.com>
2025-12-19 08:43:25 +0100

    Upgrade OFT version

--- ci/trace-requirements.sh
+++ ci/trace-requirements.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-oft_version="3.8.0"
+oft_version="4.2.0"
 
 base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 readonly base_dir
@@ -12,7 +12,7 @@ readonly oft_jar="$HOME/.m2/repository/org/itsallcode/openfasttrace/openfasttrac
 
 if [ ! -f "$oft_jar" ]; then
     echo "Downloading OpenFastTrace $oft_version"
-    mvn --batch-mode org.apache.maven.plugins:maven-dependency-plugin:3.3.0:get -Dartifact=org.itsallcode.openfasttrace:openfasttrace:$oft_version
+    mvn --batch-mode org.apache.maven.plugins:maven-dependency-plugin:3.9.0:get -Dartifact=org.itsallcode.openfasttrace:openfasttrace:$oft_version
 fi
 
 # Trace all
